a7bc21957f69f448fbcebe51bd3b0ff754e73730
BimsaraBodaragama <bodaragamadb.office@gmail.com>
2026-04-07 14:03:20 +0530

    Revert invalid org validation and servlet branch fix

--- components/org.wso2.carbon.identity.context.rewrite.valve/src/main/java/org/wso2/carbon/identity/context/rewrite/valve/TenantContextRewriteValve.java
+++ components/org.wso2.carbon.identity.context.rewrite.valve/src/main/java/org/wso2/carbon/identity/context/rewrite/valve/TenantContextRewriteValve.java
@@ -100,7 +100,6 @@ public class TenantContextRewriteValve extends ValveBase {
         boolean isWebApp = false;
         boolean isOrgQualifiedPathFlow = false;
         String effectiveTenantDomain = null;
-        String urlTenantDomain = null;
 
         /* If an organization under the super tenant is accessed with organization qualified URL, it is prefixed
            with super tenant domain qualifier. /o/... -> /t/<carbon.super>/o/... */
@@ -190,7 +189,6 @@ public class TenantContextRewriteValve extends ValveBase {
                     }
                     isOrgQualifiedPathFlow = true;
                     effectiveTenantDomain = subOrgTenantDomain;
-                    urlTenantDomain = tenantDomainFromUrl;
                     PrivilegedCarbonContext.startTenantFlow();
                     PrivilegedCarbonContext.getThreadLocalCarbonContext()
                             .setTenantDomain(subOrgTenantDomain, true);
@@ -218,7 +216,6 @@ public class TenantContextRewriteValve extends ValveBase {
                         }
                         isOrgQualifiedPathFlow = true;
                         effectiveTenantDomain = subOrgTenantDomain;
-                        urlTenantDomain = tenantDomainFromUrl;
                         PrivilegedCarbonContext.startTenantFlow();
                         PrivilegedCarbonContext.getThreadLocalCarbonContext()
                                 .setTenantDomain(subOrgTenantDomain, true);
@@ -289,7 +286,7 @@ public class TenantContextRewriteValve extends ValveBase {
                         requestURI = requestURI.replace(carbonWebContext + "/", "");
                     }
                     //Servlet
-                    requestURI = requestURI.replace("/t/" + (isOrgQualifiedPathFlow ? urlTenantDomain : tenantDomain), "");
+                    requestURI = requestURI.replace("/t/" + tenantDomain, "");
                     String appResidentOrgId =
                             PrivilegedCarbonContext.getThreadLocalCarbonContext()
                                     .getApplicationResidentOrganizationId();
